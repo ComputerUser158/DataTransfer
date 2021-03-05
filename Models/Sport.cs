f12e013e67e31d76268f609093ddfb8494077c38
ComputerUser158 <oneringraw@gmail.com>
2021-03-04 23:49:36 -0600

    Add project files.

---
+++ Models/Sport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dataTransfer.Models
+{
+    public class Sport
+    {
+        public string SportID { get; set; }
+        public string SportName { get; set; }
+    }
+}
